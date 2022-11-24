401227f00bfbff893274e0a347f5c395c61e108c
Ashley Ramsey <dusty1boy1984@gmail.com>
2022-11-24 15:13:11 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,54 @@
+const axios = require("../utils/axios");
+const BASE_URL = "http://localhost:5000";
+
+function index() {
+  const url = `${BASE_URL}/constellations`;
+    axios.get(url)
+  .then((respobse) => {
+    console.log(response.data);
+  });
+}
+
+function create(body) {
+  const url = `${BASE_URL}/constellations`;
+  axios.post(url, body)
+  .then((response) => {
+    console.log(response.data);
+  });
+}
+
+
+
+
+function create(body) {
+  const url = `${BASE_URL}/constellations`;
+  axios.post(url, body)
+  .then((response) => {
+    console.log(response.data);
+  });
+}
+
+function update(id, body) {
+  const url = `${BASE_URL}/constellations/${id}`;
+  axios.put(url, body)
+  .then((response) => {
+    console.log(response.data);
+  });
+}
+
+function destory(id) {
+  const url = `${BASE_URL}/constellations/${id}`;
+  axios.delete(url)
+  .then((response) => {
+    console.log(response.data);
+  });
+}
+
+
+module.exports = {
+  index,
+  create,
+  show,
+  update,
+  destroy,
+};
